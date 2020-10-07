3025f42a11c87ab3bce5dce7d0dcc57471cb1bb9
Mario <martzm4118@gmail.com>
2020-10-07 07:52:01 -0500

    cambio2

--- Prueba1.java
+++ Prueba1.java
@@ -13,5 +13,6 @@ public class Prueba1 {
     public static void main(String[] args) {
         System.out.println("Mario");
         System.out.println("Martínez");
+        System.out.println("Maldonado");
     }
 }
